b0160380c3a1e06532a6948ef9754e84d012f51b
dev <dev@localhost.localdomain>
2021-01-18 21:14:30 +0800

    项目http环境测试

---
+++ oj-project/test/test-httplib/test.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "httplib.h"
+
+void func(const httplib::Request& req, httplib::Response& resp)
+{
+      printf("recv abc\n");
+
+}
+
+int main()
+{
+
+   //创建httplib当中的server类对象， 使用该类对象， 模拟创建一个http服务器
+   httplib::Server svr;
+      
+   svr.Get("/abc", func);
+      
+   svr.listen("0.0.0.0", 18988);
+   return 0;
+}
